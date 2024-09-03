6ea909d301dd6797b790d61de0e5a844f3ca95e2
JaviserAS <javierastc@gmail.com>
2024-09-03 21:30:30 +0200

    Homework 1 - Build on Solana Rise in Course. - Updated Increment & Decrement functions, they both accept now a parameter passed with value to increment/decrement, reset to 0 in case   of attempting decrement the counter to negative value.

--- src/lib.rs
+++ src/lib.rs
@@ -33,11 +33,15 @@ pub fn process_instruction(
     let mut counter_account = CounterAccount::try_from_slice(&account.data.borrow())?;
 
     match instruction {
-        CounterInstructions::Increment => {
-            counter_account.counter += 1;
+        CounterInstructions::Increment(args) => {
+            counter_account.counter += args.value;
         }
-        CounterInstructions::Decrement => {
-            counter_account.counter -= 1;
+        CounterInstructions::Decrement(args) => {
+            if counter_account.counter >= args.value {
+                counter_account.counter -= args.value;
+            } else{
+                counter_account.counter = 0;
+            }
         }
         CounterInstructions::Update(args) => {
             counter_account.counter = args.value;
@@ -78,18 +82,24 @@ mod test {
         );
 
         let accounts = vec![account];
-        let increment_instruction_data = vec![0];
-        let decrement_instruction_data = vec![1];
+        let mut increment_instruction_data = vec![0];
+        let mut decrement_instruction_data = vec![1];
         let mut update_instruction_data = vec![2];
         let reset_instruction_data = vec![3];
+
+        let increment_value = 50u32;
+        increment_instruction_data.extend_from_slice(&increment_value.to_le_bytes());
 
         process_instruction(&program_id, &accounts, &increment_instruction_data).unwrap();
         assert_eq!(
             CounterAccount::try_from_slice(&accounts[0].data.borrow())
                 .unwrap()
                 .counter,
-            1
+            50
         );
+
+        let decrement_value = 53u32;
+        decrement_instruction_data.extend_from_slice(&decrement_value.to_le_bytes());
 
         process_instruction(&program_id, &accounts, &decrement_instruction_data).unwrap();
         assert_eq!(
